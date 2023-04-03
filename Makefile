6623f2eac1cb384bd77edb4a4e8cbe10c7dad728
DK <duckhuejs@gmail.com>
2023-04-04 00:14:51 +0700

    refactor: apply cqrs & port adapter & repository pattern

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 include .env
 
 .PHONY: bootstrap
-bootstrap: openapi
+bootstrap: build_oapi_codegen openapi
 
 .PHONY: openapi
 openapi: openapi_http openapi_js merge_openapi_docs
 
+.PHONY: build_oapi_codegen
+build_oapi_codegen:
+	@tput setaf 2;echo "build oapi-codegen image"
+	@docker build -f docker/oapi-codegen/Dockerfile -t oapi-codegen .
+
 .PHONY: merge_openapi_docs
 merge_openapi_docs:
-	@echo "merge openapi doc"
+	@tput setaf 2;echo "merge openapi doc"
 	@docker run --rm -v $(shell pwd)/api/openapi:/spec	redocly/cli join \
 	/spec/demo.yaml \
 	/spec/user.yaml \
@@ -16,12 +21,12 @@ merge_openapi_docs:
 
 .PHONY: openapi_http
 openapi_http:
-	@echo "generate http server"
+	@tput setaf 2;echo "generate http server"
 	@./scripts/openapi/generate_server.sh demo internal/demo/ports ports
 	@./scripts/openapi/generate_server.sh user internal/user/ports ports
 
 .PHONY: openapi_js
 openapi_js:
-	@echo "generate http client"
+	@tput setaf 2;echo "generate http client"
 	@./scripts/openapi/generate_client.sh demo
 	@./scripts/openapi/generate_client.sh user
--- scripts/openapi/generate_server.sh
+++ scripts/openapi/generate_server.sh
@@ -5,10 +5,26 @@ readonly service="$1"
 readonly output_dir="$2"
 readonly package="$3"
 
-docker run -ti --rm -v $(pwd):/mnt oapi-codegen oapi-codegen -generate types -o "/mnt/$output_dir/openapi_types.gen.go" -package "$package" "/mnt/api/openapi/$service.yaml"
+docker run -ti --rm -v $(pwd):/mnt oapi-codegen oapi-codegen \
+  -generate types \
+  -o "/mnt/$output_dir/openapi_types.gen.go" \
+  -package "$package" \
+  "/mnt/api/openapi/$service.yaml"
 
-docker run -ti --rm -v $(pwd):/mnt oapi-codegen oapi-codegen -generate chi-server -o "/mnt/$output_dir/openapi_api.gen.go" -package "$package" "/mnt/api/openapi/$service.yaml"
+docker run -ti --rm -v $(pwd):/mnt oapi-codegen oapi-codegen \
+  -generate chi-server \
+  -o "/mnt/$output_dir/openapi_api.gen.go" \
+  -package "$package" \
+  "/mnt/api/openapi/$service.yaml"
 
-docker run -ti --rm -v $(pwd):/mnt oapi-codegen oapi-codegen -generate types -o "/mnt/internal/common/client/$service/openapi_types.gen.go" -package "$service" "/mnt/api/openapi/$service.yaml"
+docker run -ti --rm -v $(pwd):/mnt oapi-codegen oapi-codegen \
+  -generate types \
+  -o "/mnt/internal/common/client/$service/openapi_types.gen.go" \
+  -package "$service" \
+  "/mnt/api/openapi/$service.yaml"
 
-docker run -ti --rm -v /$(pwd):/mnt oapi-codegen oapi-codegen -generate client -o "/mnt/internal/common/client/$service/openapi_client_gen.go" -package "$service" "/mnt/api/openapi/$service.yaml"
+docker run -ti --rm -v /$(pwd):/mnt oapi-codegen oapi-codegen \
+  -generate client \
+  -o "/mnt/internal/common/client/$service/openapi_client_gen.go" \
+  -package "$service" \
+  "/mnt/api/openapi/$service.yaml"
